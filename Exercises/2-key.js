11e1330f89dc9224734e28d9f02cb4698fd0afb7
azozulya <bbbom@mail.ru>
2019-11-01 13:58:41 +0200

    some fix

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
 const generateKey = (length, possible) => {
   const max = possible.length - 1;
   let i = 0, str = '';
   while (i < length) {
-    const randomNum = random(0, max);
-    str += possible[randomNum];
+    const idx = Math.floor(Math.random() * max);
+    str += possible[idx];
     i++;
   }
   return str;
